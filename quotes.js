83e5e8da7b981c488c399f6df0fffde36f760e11
VenusCW <mz.ve513@gmail.com>
2021-02-11 18:38:08 -0500

    adding

---
+++ quotes.js
@@ -0,0 +1,7 @@
+const quotes = [
+   " The computer was born to solve problems that did not exist before - Bill Gates",
+   " The more you know, the more you realize you know nothing - Socrates",
+   "Tell me and I forget.  Teach me and I remember.  Involve me and I learn",
+   "Real knowledge is to know the extent of one’s ignorance",
+   "Getting information off the Internet is like taking a drink from a fire hydrant",
+]
